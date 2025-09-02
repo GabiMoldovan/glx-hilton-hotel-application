7d9dc9eb8bef40068cadbfa619f4f40cba746fe3
Gabi Moldovan <68482473+GabiMoldovan@users.noreply.github.com>
2025-09-02 20:13:14 +0300

    aadded the script that creates the tabels in the data base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import org.example.database.PostgreSQLDatabase;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //System.out.println("Goodbye World!");
+
+
+
     }
 }
